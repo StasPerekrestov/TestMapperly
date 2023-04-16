0e642e9a2eed5d4608717bb7057e218a975911aa
Stanislav Perekrestov <sperekrestov@dmvdesk.com>
2023-04-16 21:51:37 +0200

    initial

---
+++ MapperlyAnalyzerDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Riok.Mapperly.Abstractions;
+
+var user = new User { Id = 123, Name = "John Doe", Address = "Mulholland drive 91" };
+var info = user.ToInfo();
+Console.WriteLine($"Hello, {info.Name}!");
+
+class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
+
+class UserInfo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+[Mapper]
+static partial class UserMapper
+{
+    public static partial UserInfo ToInfo(this User user);
+}
